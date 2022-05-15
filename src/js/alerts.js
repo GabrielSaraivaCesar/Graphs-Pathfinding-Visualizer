ca24f24c69b053d537702e885e5519f01eb5dc03
saraiva <saraiva@pop-os.localdomain>
2022-05-15 02:20:29 -0300

    Adding feedback alerts

---
+++ src/js/alerts.js
@@ -0,0 +1,25 @@
+
+const alertElement = document.querySelector('.alert');
+const alertTextElement = alertElement.querySelector('.alert-text');
+let closeEvent = null;
+
+function showAlert(status, text) {
+    clearTimeout(closeEvent);
+    alertElement.style.bottom = "0px";
+
+    alertElement.setAttribute("data-status", status);
+    alertTextElement.textContent = text;
+    alertElement.classList.add("show");
+
+    closeEvent = setTimeout(() => {
+        closeAlert();
+    }, 4000);
+}
+
+function closeAlert() {
+    alertElement.style.bottom = "-100px";
+    clearTimeout(closeEvent);
+    closeEvent = setTimeout(() => {
+        alertElement.classList.remove("show");
+    }, 500);
+}
